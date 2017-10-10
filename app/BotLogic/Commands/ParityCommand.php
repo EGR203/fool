f6859230cedb16fefc6ce90855072ea6fc7b7c42
EGR203 <novikov46en@gmail.com>
2017-10-10 17:37:06 +1000

    add fuck off command

--- app/BotLogic/Commands/ParityCommand.php
+++ app/BotLogic/Commands/ParityCommand.php
@@ -7,14 +7,16 @@ use Carbon\Carbon;
 
 class ParityCommand extends Command {
 	public function execute(): string {
-		$week_of_year = DateTimeUtils::getWeekOfStudy();
+		$week_of_study = DateTimeUtils::getWeekOfStudy();
+		$week_of_year = Carbon::now()->weekOfYear;
 		$is_odd = DateTimeUtils::isOddWeekOfStudy();
-		$msg = $is_odd ? "Сейчас нечетная неделя" : "Сейчас четная неделя";
-		$msg .= " ({$week_of_year})";
+		$msg = $is_odd ? "Сейчас нечетная учебная неделя" : "Сейчас четная учебная неделя";
+		$msg .= " : #{$week_of_study} \\n И #{$week_of_year} неделя в году";
+
 		return $msg;
 	}
 
 	public  static function getInfo(): string {
-		return "Показывает четность текущей недели";
+		return "Четность текущей учебной недели и её номер";
 	}
 }
