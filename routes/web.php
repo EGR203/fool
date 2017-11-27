7762bacf6ea4a74681ff32786d3e1e7a2e33864f
EGR203 <novikov46en@gmail.com>
2017-11-27 17:27:45 +1000

    change url to vk response

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/callback', 'VkController@index')->name('vkbot');
+Route::post('/vk', 'VkController@index')->name('vkbot');
 
 Route::post('/callback/{name}', 'ProxyController@doProxy');
 Route::post('/proxy/reg/{name}', 'ProxyController@register');
