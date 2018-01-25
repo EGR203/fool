781b5b6f0a660c17fc53fab95a85139ee9c4f6da
EGR203 <novikov46en@gmail.com>
2018-01-25 10:07:19 +1000

    change header request proxy

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -39,19 +39,48 @@ class ProxyController extends Controller {
 			$url = $proxy->ip . $proxy->path;
 		}
 
+		$boundary = uniqid();
+		$delimiter = '-------------' . $boundary;
+		$post_data = $this->build_data_files($boundary, $request->all());
+
+
 		$ch = curl_init( $url );
+
 		curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0" );
 		curl_setopt( $ch, CURLOPT_POST, true );
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $request->all() ) );
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, $post_data );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+			"Content-Type: multipart/form-data; boundary=" . $delimiter
+		));
 
 		$response = curl_exec( $ch );
 		curl_close( $ch );
 		if ( ! $response ) {
 			return '<h1> ERROR DURING CURL (Have not response)</h1>';
 		}
+		@\Log::info("Url: $url :::: Request: " . print_r($request->all(), 1 ) . " :::: Response: $response");
 
 		return $response;
+	}
+
+	protected function build_data_files($boundary, $fields){
+		$data = '';
+		$eol = "\r\n";
+
+		$delimiter = '-------------' . $boundary;
+
+		foreach ($fields as $name => $content) {
+			$data .= "--" . $delimiter . $eol
+			         . 'Content-Disposition: form-data; name="' . $name . "\"".$eol.$eol
+			         . $content . $eol;
+		}
+
+
+		$data .= "--" . $delimiter . "--".$eol;
+
+
+		return $data;
 	}
 }
